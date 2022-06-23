2572872d1cc093d8f512a5ddd39e85b352b048ac
Juhyun Choi <julia.juhyun@gmail.com>
2022-06-23 16:44:43 +0900

    matplotlib

--- pages/crimeLocation.py
+++ pages/crimeLocation.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas
 import os
 
-import matplotlib.pyplot as plt
-import matplotlib as mpl
+# import matplotlib.pyplot as plt
+# import matplotlib as mpl
 
 def drawCrimeLocationPage():
     current_path = os.path.abspath(os.path.dirname(__file__))
@@ -141,7 +141,7 @@ def drawCrimeLocationPage():
     # import matplotlib.pyplot as plt
 
     # fig = plt.figure(figsize=(500, 500), dpi=2, facecolor='red',edgecolor='black')
-    fig = plt.figure(figsize=(10,10))
+    # fig = plt.figure(figsize=(10,10))
     # # 전체 창을 가로 1칸, 세로 1칸으로 쪼개고 그 중 첫 번째 칸에 ax라는 이름의 axes를 생성한다.
     # ax1 = fig.add_subplot(1, 2, 1)
     # # 전체 창을 가로 1칸, 세로 2칸으로 쪼개고, 그 중 첫 번째 칸에 ax1, 두 번째 칸에 ax2를 생성한다.
@@ -168,19 +168,224 @@ def drawCrimeLocationPage():
     # ax4.plot([5, 10, 20, 10])  # 네 번째에 그림을 그린다.
     # ax4.set_title('baz')  # 네 번째에 타이틀을 추가한다.
 
-    csv_read.set_index('범죄분류', inplace=True)
+    # csv_read.set_index('범죄분류', inplace=True)
+    #
+    # font1 = {'family': 'serif',
+    #          'color': 'b',
+    #          'weight': 'bold',
+    #          'size': 14
+    #          }
+    #
+    # font2 = {'family': 'fantasy',
+    #          'color': 'deeppink',
+    #          'weight': 'normal',
+    #          'size': 'xx-large'
+    #          }
+    # graph = plt.subplot(1, 1, 1)
+    # # graph.set_xlabel('aaa')
+    # # graph.set_ylabel('bbb')
+    # plt.xlabel('X-Axis', labelpad=15, fontdict=font1)  # loc='right'
+    # plt.ylabel('Y-Axis', labelpad=20, fontdict=font2)  # loc='top'
+    # graph.set_title('cccc')
+    # graph.plot(csv_read['서울_종로'])
+    #
+    # plt.show()
 
-    plt.rcParams['font.family'] = 'AppleGothic'
-    # mpl.font_manager._rebuild()
+    # https://wikidocs.net/book/5011
+    # 기본 그래프 그리기
+    # import matplotlib.pyplot as plt
+    #
+    # plt.plot([1, 3, 7, 4])
+    # plt.show()
+
+    # import matplotlib.pyplot as plt
+    #
+    # plt.plot([1, 3, 7, 4], [1, 4, 9, 16])
+    # plt.show()
+
+    # 스타일 지정하기
+    # import matplotlib.pyplot as plt
+    #
+    # plt.plot([1, 3, 7, 4], [1, 4, 9, 16], 'r*')  # red에 O로 표시, r-, r*, ro
+    # plt.axis([0, 8, 0, 20])   # x축: 0~8, y축: 0~20
+    # plt.show()
+
+    # 여러 개의 그래프 그리기
+    # import matplotlib.pyplot as plt
+    # import numpy as np
+    #
+    # # 200ms 간격으로 균일하게 샘플된 시간
+    # t = np.arange(0., 5., 0.2)
+    #
+    # # 빨간 대쉬, 파란 사각형, 녹색 삼각형
+    # plt.plot(t, t, 'r--', t, t ** 2, 'bs', t, t ** 3, 'g^')
+    # plt.show()
+
+    # 레이블 사용하기
+    # import matplotlib.pyplot as plt
+    #
+    # plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
+    # plt.xlabel('X-Label')
+    # plt.ylabel('Y-Label')
+    # plt.show()
+
+    # 레이블 패딩
+    # import matplotlib.pyplot as plt
+    #
+    # plt.plot([1, 2, 3, 4], [2, 3, 5, 10])
+    # plt.xlabel('X-Axis', labelpad=15)
+    # plt.ylabel('Y-Axis', labelpad=20)
+    # plt.show()
+
+    # font 정하기
+    # import matplotlib.pyplot as plt
+    #
+    # font1 = {'family': 'serif',
+    #          'color': 'b',
+    #          'weight': 'bold',
+    #          'size': 14
+    #          }
+    #
+    # font2 = {'family': 'fantasy',
+    #          'color': 'deeppink',
+    #          'weight': 'normal',
+    #          'size': 'xx-large'
+    #          }
+    #
+    # plt.plot([1, 2, 3, 4], [2, 3, 5, 10])
+    # plt.xlabel('X-Axis', labelpad=15, fontdict=font1)
+    # plt.ylabel('Y-Axis', labelpad=20, fontdict=font2)
+    # plt.show()
+
+    # 라벨 위치 정하기
+    # import matplotlib.pyplot as plt
+    #
+    # plt.plot([1, 2, 3, 4], [2, 3, 5, 10])
+    # plt.xlabel('X-Axis', loc='right')
+    # plt.ylabel('Y-Axis', loc='top')
+    # plt.show()
+
+    # 선 설명하기
+    # import matplotlib.pyplot as plt
+    #
+    # plt.plot([1, 2, 3, 4], [2, 3, 5, 10], label='Price ($)')
+    # plt.xlabel('X-Axis')
+    # plt.ylabel('Y-Axis')
+    # plt.legend()
+    #
+    # plt.show()
+
+    # 선 설명의 위치 지정하기
+    # import matplotlib.pyplot as plt
+    #
+    # plt.plot([1, 2, 3, 4], [2, 3, 5, 10], label='Price ($)')
+    # plt.xlabel('X-Axis')
+    # plt.ylabel('Y-Axis')
+    # # plt.legend(loc=(0.0, 0.0))
+    # # plt.legend(loc=(0.5, 0.5))
+    # plt.legend(loc=(1.0, 1.0))
+    # # plt.legend(loc='lower right')
+    #
+    # plt.show()
+
+    # 열 개수 지정하기
+    # import matplotlib.pyplot as plt
+    #
+    # plt.plot([1, 2, 3, 4], [2, 3, 5, 10], label='Price ($)')
+    # plt.plot([1, 2, 3, 4], [3, 5, 9, 7], label='Demand (#)')
+    # plt.xlabel('X-Axis')
+    # plt.ylabel('Y-Axis')
+    # # plt.legend(loc='best')          # ncol = 1
+    # plt.legend(loc='best', ncol=2)  # ncol = 2
+    #
+    # plt.show()
+
+    # 범위정하기
+    # import matplotlib.pyplot as plt
+    #
+    # plt.plot([1, 2, 3, 4], [2, 3, 5, 10])
+    # plt.xlabel('X-Axis')
+    # plt.ylabel('Y-Axis')
+    # plt.xlim([0, 5])  # X축의 범위: [xmin, xmax]
+    # plt.ylim([0, 20])  # Y축의 범위: [ymin, ymax]
+    #
+    # plt.show()
 
 
-    graph = plt.subplot(1, 1, 1)
-    graph.set_xlabel('aaa')
-    graph.set_ylabel('bbb')
-    graph.set_title('cccc')
-    graph.plot(csv_read['서울_종로'])
+    # 선 종류 지정하기
+    # import matplotlib.pyplot as plt
+    #
+    # plt.plot([1, 2, 3], [4, 4, 4], '-', color='C0', label='Solid')
+    # plt.plot([1, 2, 3], [3, 3, 3], '--', color='C0', label='Dashed')
+    # plt.plot([1, 2, 3], [2, 2, 2], ':', color='C0', label='Dotted')
+    # plt.plot([1, 2, 3], [1, 1, 1], '-.', color='C0', label='Dash-dot')
+    # plt.xlabel('X-Axis')
+    # plt.ylabel('Y-Axis')
+    # plt.axis([0.8, 3.2, 0.5, 5.0])
+    # plt.legend(loc='upper right', ncol=4)
+    # plt.show()
+    #
+    # 또는.
+    # import matplotlib.pyplot as plt
+    #
+    # plt.plot([1, 2, 3], [4, 4, 4], linestyle='solid', color='C0', label="'solid'")
+    # plt.plot([1, 2, 3], [3, 3, 3], linestyle='dashed', color='C0', label="'dashed'")
+    # plt.plot([1, 2, 3], [2, 2, 2], linestyle='dotted', color='C0', label="'dotted'")
+    # plt.plot([1, 2, 3], [1, 1, 1], linestyle='dashdot', color='C0', label="'dashdot'")
+    # plt.xlabel('X-Axis')
+    # plt.ylabel('Y-Axis')
+    # plt.axis([0.8, 3.2, 0.5, 5.0])
+    # plt.legend(loc='upper right', ncol=4)
+    # plt.tight_layout()
+    # plt.show()
 
-    plt.show()
+    # 선 끝모양 지정하기
+    # import matplotlib.pyplot as plt
+    #
+    # plt.plot([1, 2, 3], [4, 4, 4], linestyle='solid', linewidth=10,
+    #          solid_capstyle='butt', color='C0', label='solid+butt')
+    # plt.plot([1, 2, 3], [3, 3, 3], linestyle='solid', linewidth=10,
+    #          solid_capstyle='round', color='C0', label='solid+round')
+    #
+    # plt.plot([1, 2, 3], [2, 2, 2], linestyle='dashed', linewidth=10,
+    #          dash_capstyle='butt', color='C1', label='dashed+butt')
+    # plt.plot([1, 2, 3], [1, 1, 1], linestyle='dashed', linewidth=10,
+    #          dash_capstyle='round', color='C1', label='dashed+round')
+    #
+    # plt.xlabel('X-Axis')
+    # plt.ylabel('Y-Axis')
+    # plt.axis([0.8, 3.2, 0.5, 5.0])
+    # plt.legend(loc='upper right', ncol=2)
+    # plt.tight_layout()
+    # plt.show()
+
+    # 눈금 표시하기
+    # import numpy as np
+    # import matplotlib.pyplot as plt
+    #
+    # x = np.arange(0, 2, 0.2)
+    #
+    # plt.plot(x, x, 'bo')
+    # plt.plot(x, x ** 2, color='#e35f62', marker='*', linewidth=2)
+    # plt.plot(x, x ** 3, color='forestgreen', marker='^', markersize=9)
+    # plt.xticks([0, 1, 2])
+    # plt.yticks(np.arange(1, 6))
+    #
+    # plt.show()
+
+    # import matplotlib.pyplot as plt
+    # import numpy as np
+    #
+    # x = np.arange(0, 2, 0.2)
+    #
+    # plt.plot(x, x, 'bo')
+    # plt.plot(x, x ** 2, color='#e35f62', marker='*', linewidth=2)
+    # plt.plot(x, x ** 3, color='springgreen', marker='^', markersize=9)
+    # plt.xticks(np.arange(0, 2, 0.2), labels=['Jan', '', 'Feb', '', 'Mar', '', 'May', '', 'June', '', 'July'])
+    # plt.yticks(np.arange(0, 7), ('0', '1GB', '2GB', '3GB', '4GB', '5GB', '6GB'))
+    #
+    # plt.show()
+
 
 
 if __name__ == "__main__":
